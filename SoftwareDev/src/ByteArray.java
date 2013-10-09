368fc75868b6967a96b87cd5b29cf80016473d39
Reuben Jacobs <reuben.jacobs1@gmail.com>
2013-10-08 22:56:43 -0400

    initial

---
+++ SoftwareDev/src/ByteArray.java
@@ -0,0 +1,46 @@
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+public class ByteArray {
+	
+	
+	public static void main(String[] args) throws UnsupportedAudioFileException, IOException {
+		ByteArray b = new ByteArray();
+		byte[] bytesOne = b.toByteArray();
+	}
+	
+	public byte[] toByteArray() throws UnsupportedAudioFileException, IOException {
+		
+		Scanner input = new Scanner(System.in);
+		String path = "";
+		
+		System.out.println("Please enter a file path");
+		//Path for the file to analyze
+		path = input.nextLine();
+		
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		AudioInputStream in = AudioSystem.getAudioInputStream(new File(path));
+		 
+		int read;
+		byte[] buff;
+		buff = new byte[1024];
+		 
+		while ((read = in.read(buff)) > 0) {
+			out.write(buff, 0, read);
+		}
+		 
+		out.flush();
+		byte[] audioBytes = out.toByteArray();
+		return audioBytes;
+
+	}
+}
+
+	
---
+++ SoftwareDev/src/FFT.java
@@ -0,0 +1,37 @@
+public class FFT {
+    
+	public byte[] computeFFT(byte[] byteArr) {
+		int N = byteArr.length;
+
+	    // base case
+	    if (N == 1) { 
+	    	return new byte[] { byteArr[0] };
+	    }
+
+	    // radix 2 Cooley-Tukey FFT
+	    if (N % 2 != 0) { 
+	    	throw new RuntimeException("N is not a power of 2"); 
+	    }
+	    
+	    byte[] even = new byte[N/2];
+	    for (int i = 0; i < N/2; i++) {
+	    	even[i] = byteArr[2*i];
+	    }
+	    byte[] evenArray = computeFFT(even);
+	    
+	    byte[] odd = new byte[N/2];
+	    for (int i = 0; i < N/2; i++) {
+	    	odd[i] = byteArr[2*i + 1];
+	    }
+	    byte[] oddArray = computeFFT(even);
+	    
+	    byte[] finalBytes = new byte[N/2];
+	    for (int i = 0; i < N/2; i++) {
+	    	double k = -2 * i * Math.PI / N;
+	    	//Do stuff here
+	    }
+	    
+	    return finalBytes;
+		
+	}
+}
